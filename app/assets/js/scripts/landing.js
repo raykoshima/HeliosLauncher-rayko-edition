f3c8b7cbc6499e466bb2e815d7fcfb489b331308
raykoshima <salonezamalone@gmail.com>
2025-12-13 20:02:23 +0700

    more image and change api

--- app/assets/js/scripts/landing.js
+++ app/assets/js/scripts/landing.js
@@ -153,8 +153,11 @@ function updateSelectedAccount(authUser) {
             username = authUser.displayName
         }
         if (authUser.uuid != null) {
-            document.getElementById('avatarContainer').style.backgroundImage = `url('https://mc-heads.net/body/${authUser.uuid}/right')`
+            document.getElementById('avatarContainer').style.backgroundImage = `url('https://mc-heads.net/avatar/${authUser.uuid}')`
         }
+        // if (authUser.uuid != null) {
+        //     document.getElementById('avatarContainer').style.backgroundImage = `url('https://mc-heads.net/body/${authUser.uuid}/right')`
+        // }
     }
     user_text.innerHTML = username
 }
